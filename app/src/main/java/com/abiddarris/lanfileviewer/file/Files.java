b7f823b1d3f2067c25ff8bea1f8a1f16e4cba8ee
Abiddarris <Abiddaris1101@gmail.com>
2023-11-16 18:47:15 +0800

    Prepare on UploadDialog

---
+++ app/src/main/java/com/abiddarris/lanfileviewer/file/Files.java
@@ -0,0 +1,20 @@
+package com.abiddarris.lanfileviewer.file;
+import java.util.List;
+
+public class Files {
+    
+    public static void getFilesTree(List<File> files, File parent) {
+        files.add(parent);
+        if(parent.isFile()) {
+            return;
+        }
+        
+        File[] children = parent.listFiles();
+        if(children == null) return;
+        
+        for(File file : files) {
+            getFilesTree(files,file);
+        }
+    }
+    
+}
--- app/src/main/java/com/abiddarris/lanfileviewer/ui/actions/UploadDialog.java
+++ app/src/main/java/com/abiddarris/lanfileviewer/ui/actions/UploadDialog.java
@@ -2,18 +2,28 @@ package com.abiddarris.lanfileviewer.ui.actions;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import com.abiddarris.lanfileviewer.R;
 import com.abiddarris.lanfileviewer.databinding.UploadDialogBinding;
 import com.abiddarris.lanfileviewer.file.File;
+import com.abiddarris.lanfileviewer.file.Files;
 import com.abiddarris.lanfileviewer.utils.BaseRunnable;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.dialog.MaterialDialogs;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class UploadDialog extends DialogFragment {
     
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
     private File[] items;
+    private Handler handler = new Handler(Looper.getMainLooper());
     private UploadDialogBinding view;
     
     public UploadDialog(File[] items) {
@@ -36,17 +46,31 @@ public class UploadDialog extends DialogFragment {
     
     @Override
     public void onStart() {
-        
         super.onStart();
+        
+        executor.submit(new UploadRunnable());
     }
     
     public class UploadRunnable extends BaseRunnable {
         
         @Override
         public void onExecute() throws Exception {
+            prepare();
             
+            List<File> files = new ArrayList<>();
+            for(File file : items) {
+                Files.getFilesTree(files, file);
+            }
         }
         
+        public void prepare() {
+        	handler.post(() -> {
+                view.name.setVisibility(View.INVISIBLE);
+                view.progressPercent.setVisibility(View.INVISIBLE);
+                    
+                view.progress.setText(R.string.preparing);    
+            });
+        }
     }
     
 }
