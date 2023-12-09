f625deef0512c034bddee4cf4c7d848d1c9d8b8e
Abiddarris <Abiddaris1101@gmail.com>
2023-12-09 20:26:55 +0800

    Sorting by name on sort by type mode when it's a folder

--- app/src/main/java/com/abiddarris/lanfileviewer/sorter/TypeSorter.java
+++ app/src/main/java/com/abiddarris/lanfileviewer/sorter/TypeSorter.java
@@ -5,10 +5,16 @@ import com.abiddarris.lanfileviewer.file.Files;
 
 public class TypeSorter extends FileSorter {
     
+    private NameSorter nameSorter = new NameSorter();
+    
     @Override
     public int compare(File first, File second) {
-        return Files.getExtension(first).toLowerCase()
+        int typeResult = Files.getExtension(first).toLowerCase()
             .compareTo(Files.getExtension(second).toLowerCase());
+        
+        if(typeResult != 0) return typeResult;
+        
+        return nameSorter.compare(first,second);
     }
     
 }
