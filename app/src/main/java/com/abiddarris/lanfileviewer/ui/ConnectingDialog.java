bf439a3e04f6e58455b7f7d77f3641e51794574d
Abiddarris <Abiddaris1101@gmail.com>
2024-03-01 16:10:15 +0800

    prevent timer from resetted if orientation changed

--- app/src/main/java/com/abiddarris/lanfileviewer/ui/ConnectingDialog.java
+++ app/src/main/java/com/abiddarris/lanfileviewer/ui/ConnectingDialog.java
@@ -6,6 +6,8 @@ import android.os.Handler;
 import android.os.Looper;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import com.abiddarris.lanfileviewer.R;
 import com.abiddarris.lanfileviewer.settings.Settings;
 import com.abiddarris.lanfileviewer.utils.BaseRunnable;
@@ -17,24 +19,17 @@ public class ConnectingDialog extends DialogFragment {
     public static final String NAME = "name";
   
     private AlertDialog dialog;
-    private HandlerLogSupport handler = new HandlerLogSupport(new Handler(Looper.getMainLooper()));
-    private int seconds;
-    private String secondString;
-    private String message;
-    private String name;
     
     @Override
     public Dialog onCreateDialog(Bundle bundle) {
-        Bundle argument = getArguments();
+        ConnectViewModel viewModel = new ViewModelProvider(this)
+            .get(ConnectViewModel.class);
         
-        name = argument.getString(NAME);
-        message = getString(R.string.connecting_dialog_desc);
-        seconds = Settings.getConnectTimeout(getContext());
-        secondString = getString(R.string.second);
+        viewModel.init(this);
         
         dialog = new MaterialAlertDialogBuilder(getContext())
             .setTitle(R.string.connecting_dialog_title)
-            .setMessage(formatMessage())
+            .setMessage(viewModel.formatMessage())
             .setNeutralButton(R.string.cancel, (p1,p2) -> {
                 dismiss();
                 getActivity().finish();
@@ -43,27 +38,74 @@ public class ConnectingDialog extends DialogFragment {
         
         setCancelable(false);
         
-        handler.postDelayed(new CountdownRunnable(), 1000);
+        viewModel.startCountDown(dialog);
         
         return dialog;
     }
     
-    private String formatMessage() {
-        return String.format(message, name, seconds, secondString);
-    }
-    
-    private class CountdownRunnable extends BaseRunnable {
+    public static class ConnectViewModel extends ViewModel {
+        
+        private AlertDialog dialog;
+        private CountdownRunnable runnable;
+        private HandlerLogSupport handler = new HandlerLogSupport(new Handler(Looper.getMainLooper()));
+        private int seconds;
+        private String secondString;
+        private String message;
+        private String name;
+        
+        private void init(ConnectingDialog dialog) {
+            if(this.dialog != null) {
+                return;
+            }
+            Bundle argument = dialog.getArguments();
+        
+            name = argument.getString(NAME);
+            message = dialog.getString(R.string.connecting_dialog_desc);
+            seconds = Settings.getConnectTimeout(dialog.getContext());
+            secondString = dialog.getString(R.string.second);
+        }
+        
+        private String formatMessage() {
+            return String.format(message, name, seconds, secondString);
+        }
+        
+        private void startCountDown(AlertDialog dialog) {
+            this.dialog = dialog;
+            
+            if(runnable != null) return;
+            runnable = new CountdownRunnable();
+        	handler.postDelayed(runnable, 1000);
+        }
+        
+        private class CountdownRunnable extends BaseRunnable {
+        
+            private boolean cancel;
+            
+            @Override
+            public void onExecute(BaseRunnable context) throws Exception {
+                super.onExecute(context);
+                
+                if(cancel) return;
+            
+                seconds--;
+                dialog.setMessage(formatMessage());
+            
+                if(seconds > 0 && !cancel)
+                    handler.postDelayed(this, 1000);
+            }
+            
+        }
         
         @Override
-        public void onExecute(BaseRunnable context) throws Exception {
-            super.onExecute(context);
+        protected void onCleared() {
+            super.onCleared();
             
-            seconds--;
-            dialog.setMessage(formatMessage());
-            
-            if(seconds > 0)
-                handler.postDelayed(this, 1000);
+            dialog = null;
+            if(runnable != null) {
+                runnable.cancel = true;
+            }
         }
+        
         
     }
     
